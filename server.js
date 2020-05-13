7ebf1a4fe6b630e96739f814b79ccedfa2a94187
AMIR ESHAQ <31240313+jamalag@users.noreply.github.com>
2020-05-13 14:05:56 -0400

    Update server.js
    You missed quite a bit of code that is required for you to exchange SDP and ICE Candidates before exchange of streams can take place.

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 
 const express = require('express');
 
-var io = require('socket.io');
+var io = require('socket.io')//;
 ({
     path:'/webrtc'
 })
@@ -26,4 +26,36 @@ let connectedPeers = new Map()
 
 peers.on('connection', socket => {
     console.log(socket.id);
+    //////// THIS IS MISSING ///////
+    socket.emit('connection-success', { success: socket.id })
+  
+    connectedPeers.set(socket.id, socket)
+  
+    socket.on('disconnect', () => {
+      console.log('disconnected')
+      connectedPeers.delete(socket.id)
+    })
+  
+    socket.on('offerOrAnswer', (data) => {
+      // send to the other peer(s) if any
+      for (const [socketID, socket] of connectedPeers.entries()) {
+        // don't send to self
+        if (socketID !== data.socketID) {
+          console.log(socketID, data.payload.type)
+          socket.emit('offerOrAnswer', data.payload)
+        }
+      }
+    })
+  
+    socket.on('candidate', (data) => {
+      // send candidate to the other peer(s) if any
+      for (const [socketID, socket] of connectedPeers.entries()) {
+        // don't send to self
+        if (socketID !== data.socketID) {
+          console.log(socketID, data.payload)
+          socket.emit('candidate', data.payload)
+        }
+      }
+    })
+    //////// THIS IS MISSING ///////
 })
